ed8d64916e118ca4d9c9876b0abfed03aea6da84
Lucas Henrique <lukas.paes18@gmail.com>
2023-01-28 15:28:33 -0300

    feature: mudando cor do background da view

--- src/screens/SplashScreen/SplashScreen.tsx
+++ src/screens/SplashScreen/SplashScreen.tsx
@@ -3,7 +3,12 @@ import {SafeAreaView, Text} from 'react-native';
 export const SplashScreen = () => {
   return (
     <SafeAreaView
-      style={{justifyContent: 'center', alignItems: 'center', flex: 1}}>
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+        backgroundColor: '#fff',
+      }}>
       <Text>SplashScreen</Text>
     </SafeAreaView>
   );
