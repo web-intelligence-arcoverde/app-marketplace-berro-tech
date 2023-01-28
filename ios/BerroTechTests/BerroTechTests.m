9d62d50328c0159fb94a8611400623fa96152982
Lucas Henrique <lukas.paes18@gmail.com>
2023-01-28 15:04:46 -0300

    feature: change name app

--- ios/BerroTechTests/BerroTechTests.m
+++ ios/BerroTechTests/BerroTechTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface AppMarketplaceBerroTechTests : XCTestCase
+@interface BerroTechTests : XCTestCase
 
 @end
 
-@implementation AppMarketplaceBerroTechTests
+@implementation BerroTechTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
