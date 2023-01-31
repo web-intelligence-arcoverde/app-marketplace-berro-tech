52ef1805424a78fdfdd09bf959456f8faf47cc00
Lucas Henrique <lukas.paes18@gmail.com>
2023-01-31 12:34:38 -0300

    feature: adicinado cores e as metricas

--- src/common/colors.ts
+++ src/common/colors.ts
@@ -1,5 +1,18 @@
 export const COLORS = {
-  'black-100': '#000',
+  primary_color: '#FFCB04',
+  secondary_color: '#A68300',
+  third_color: '#F7EBBA',
 
-  'yellow-100': '#FFCB04',
+  primary_text_color: '#403200',
+  secondary_text_color: '#A68300',
+  third_text_color: '#735B00',
+
+  background_color: '#F8F3DF',
+
+  black_color_50: '#201e1e',
+  black_color_70: '#282828',
+
+  white_color_100: '#fff',
+
+  gray_color_70: '#D1D1D1',
 };
---
+++ src/utils/metrics.ts
@@ -0,0 +1,65 @@
+import {scale} from './';
+
+export const $METRICS = {
+  //TopBar
+  topBarHeight: scale(64),
+
+  // Fonts
+  titleFontSizeBig: 14,
+
+  // Paddings
+  topBarContainerPadding: 30,
+
+  // Icon and Images Size
+  iconSmallWidth: 16,
+  iconSmallHeight: 16,
+
+  imageSmallWidth: 64,
+  imageSmallHeight: 64,
+
+  // elements size
+  pillsWidth: 130,
+  pillsHeight: 42,
+
+  // Border sizes
+  pillsRadius: 28,
+
+  // Icons size
+  smallIconWidth: 18,
+  smallIconHeight: 18,
+  mediumIconHeight: scale(40),
+  mediumIconWidth: scale(40),
+  largeIconHeight: scale(60),
+  largeIconWidth: scale(60),
+
+  // Font Sizes
+  font: {
+    small: scale(14),
+    regular: scale(16),
+    big: scale(20),
+    large: scale(24),
+    xLarge: scale(32),
+  },
+
+  lineHeight: {
+    small: scale(16),
+    medium: scale(18),
+    xMedium: scale(24),
+    large: scale(32),
+  },
+
+  // Spacing
+  insideNoSpacing: 0,
+  insideSpacingXSmall: scale(8),
+  insideSpacingSmall: scale(16),
+  insideSpacingMedium: scale(24),
+  insideSpacingXMedium: scale(32),
+  insideSpacingLarge: scale(40),
+
+  dividerNoSpacing: 0,
+  dividerSpacingXSmall: scale(12),
+  dividerSpacingSmall: scale(20),
+  dividerSpacingMedium: scale(36),
+  dividerSpacingBig: scale(60),
+  dividerSpacingLarge: scale(120),
+};
