72632e374e4eca9be7f3e6f8f0522ca4da39e68a
Lucas Henrique <lukas.paes18@gmail.com>
2023-02-17 12:32:16 -0300

    feature: add fonts and logo

--- src/common/colors.ts
+++ src/common/colors.ts
@@ -1,18 +1,54 @@
 export const COLORS = {
-  primary_color: '#FFCB04',
-  secondary_color: '#A68300',
-  third_color: '#F7EBBA',
+  dark: {
+    _01: '#17161E',
+    _02: '#282733',
+    _03: '#3A3847',
+    _04: '#4D4B5C',
+  },
 
-  primary_text_color: '#403200',
-  secondary_text_color: '#A68300',
-  third_text_color: '#735B00',
-
-  background_color: '#F8F3DF',
-
-  black_color_50: '#201e1e',
-  black_color_70: '#282828',
-
-  white_color_100: '#fff',
-
-  gray_color_70: '#D1D1D1',
+  gray: {
+    _01: '#605E70',
+    _02: '#737185',
+    _03: '#888599',
+    _04: '#9C99AD',
+    _05: '#B1AEC2',
+  },
+  light: {
+    _01: '#CBC8DB',
+    _02: '#F2F1F7',
+    _03: '#FBFAFF',
+    _04: '#FAFAFC',
+    _05: '#ffffff',
+  },
+  brand_dark: {
+    _01: '#403200',
+    _02: '#735B00',
+    _03: '#A68300',
+    _04: '#D9B11A',
+  },
+  brand_light: {
+    _01: '#FFCB04',
+    _02: '#F7EBBA',
+    _03: '#F8F3DF',
+    _04: '#FCFCFA',
+  },
+  sub_brand: {
+    _01: '#001540',
+    _02: '#1B5DE0',
+    _03: '#F3F7FD',
+    _04: '#FBFCFE',
+  },
+  auxiliary: {
+    green_confirm: '#25D366',
+    red_negative: '#D33C25',
+    red_state: '#FA5C5C',
+    orange_state: '#FF8C4D',
+    green_state: '#408F70',
+    blue_state: '#4D7AD1',
+  },
+  opacity: {
+    blue_01: 'rgba(27, 93, 224, 0.1)',
+    blue_05: 'rgba(27, 93, 224, 0.05)',
+    blue_08: 'rgba(27, 93, 224, 0.08)',
+  },
 };
