6d2a155fec51d8011e716d116d292b0cc5bec9c3
Lucas Henrique <lukas.paes18@gmail.com>
2023-03-09 12:45:56 -0300

    feature: tab navigation

--- src/routers/bottom.router.tsx
+++ src/routers/bottom.router.tsx
@@ -1,38 +1,45 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {COLORS} from '../common';
 
 const Tab = createBottomTabNavigator();
 
-import {ProfileScreen, HomeScreen, BusinessScreen} from '../screens/';
+import {IconComponent} from '../components';
 
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import {ProfileScreen, HomeScreen, BusinessScreen} from '../screens/';
 
 export const DashboardBottomNavigation = () => {
   return (
     <Tab.Navigator
       initialRouteName={'Perfil'}
       screenOptions={({route}) => ({
+        tabBarStyle: {
+          height: 100,
+        },
+
+        tabBarItemStyle: {justifyContent: 'flex-start', paddingTop: 10},
+
         headerShown: false,
-        tabBarIcon: ({focused, color, size}) => {
+        tabBarHideOnKeyboard: true,
+        tabBarLabelStyle: {fontFamily: 'inter', fontSize: 14},
+        tabBarIcon: ({focused}) => {
           let iconName;
 
           if (route.name === 'Home') {
-            iconName = focused
-              ? 'ios-information-circle'
-              : 'ios-information-circle-outline';
-          } else if (route.name === 'Negocios') {
-            iconName = focused ? 'ios-list' : 'ios-list-outline';
+            iconName = !focused ? 'home-icon' : 'home-outlined-icon';
+          } else if (route.name === 'Negócios') {
+            iconName = !focused ? 'business-icon' : 'business-outlined-icon';
           } else if (route.name === 'Perfil') {
-            iconName = focused ? 'ios-list' : 'ios-list-outline';
+            iconName = !focused ? 'profile-icon' : 'profile-outlined-icon';
           }
 
           // You can return any component that you like here!
-          return <Ionicons name={iconName} size={size} color={color} />;
+          return <IconComponent icon={iconName} />;
         },
-        tabBarActiveTintColor: 'tomato',
-        tabBarInactiveTintColor: 'gray',
+        tabBarActiveTintColor: `${COLORS.sub_brand._02}`,
+        tabBarInactiveTintColor: `${COLORS.gray._01}`,
       })}>
       <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Negocios" component={BusinessScreen} />
+      <Tab.Screen name="Negócios" component={BusinessScreen} />
       <Tab.Screen name="Perfil" component={ProfileScreen} />
     </Tab.Navigator>
   );
