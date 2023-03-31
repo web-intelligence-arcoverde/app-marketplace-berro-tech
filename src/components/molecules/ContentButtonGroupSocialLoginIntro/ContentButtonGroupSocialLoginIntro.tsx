d1e30b895ebeee6a9a8c82a4b11dcd30fe191086
Lucas Henrique <lukas.paes18@gmail.com>
2023-03-31 02:06:30 -0300

    feature: sign in facebook

--- src/components/molecules/ContentButtonGroupSocialLoginIntro/ContentButtonGroupSocialLoginIntro.tsx
+++ src/components/molecules/ContentButtonGroupSocialLoginIntro/ContentButtonGroupSocialLoginIntro.tsx
@@ -1,5 +1,6 @@
 import {Button} from '../..';
 import {useNavigationHook} from '../../../hooks';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
 
 import {Container} from './style';
 
@@ -9,9 +10,52 @@ import {
   signInRequestGoogle,
 } from '../../../store/reducer/auth/actions';
 
+import {LoginManager, AccessToken} from 'react-native-fbsdk-next';
+import auth from '@react-native-firebase/auth';
+
 export const ContentButtonGroupSocialLoginIntro = () => {
   const {goToRouter} = useNavigationHook();
   const dispatch = useAppDispatch();
+
+  async function onFacebookButtonPress() {
+    // Attempt login with permissions
+    const result = await LoginManager.logInWithPermissions([
+      'public_profile',
+      'email',
+    ]);
+
+    if (result.isCancelled) {
+      throw 'User cancelled the login process';
+    }
+
+    // Once signed in, get the users AccesToken
+    const data = await AccessToken.getCurrentAccessToken();
+
+    if (!data) {
+      throw 'Something went wrong obtaining access token';
+    }
+
+    // Create a Firebase credential with the AccessToken
+    const facebookCredential = auth.FacebookAuthProvider.credential(
+      data.accessToken,
+    );
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(facebookCredential);
+  }
+
+  async function onGoogleButtonPress() {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({showPlayServicesUpdateDialog: true});
+    // Get the users ID token
+    const {idToken} = await GoogleSignin.signIn();
+
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+    // Sign-in the user with the credential
+    return auth().signInWithCredential(googleCredential);
+  }
 
   return (
     <Container>
@@ -31,7 +75,7 @@ export const ContentButtonGroupSocialLoginIntro = () => {
         title="Entrar com facebook"
         variant="outlined"
         icon="facebook"
-        onPress={() => dispatch(signInRequestFacebook())}
+        onPress={() => onFacebookButtonPress()}
       />
     </Container>
   );
